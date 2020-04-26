ad432694caa90a89f6e7b2b184012b7b64839cb8
Durgesh Kumar <durgeshkumar@Durgeshs-MacBook-Air.local>
2020-04-26 22:21:20 +0530

    Setting Dynamic Attribute value

--- counter-app/src/components/counterComponent.jsx
+++ counter-app/src/components/counterComponent.jsx
@@ -3,12 +3,14 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     counter: 0,
+    imageUrl: "https://picsum.photos/200",
   };
   render() {
     return (
       <React.Fragment>
+        <img src={this.state.imageUrl} alt="" />
         <span>{this.state.counter}</span>
-        <button> Counter </button>
+        <button> {this.state.counter} </button>
       </React.Fragment>
     );
   }
