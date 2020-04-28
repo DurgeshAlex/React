9e651d6abd59914b52dd41b5569dc650b3b8ea07
Durgesh Kumar <durgeshkumar@Durgeshs-MacBook-Air.local>
2020-04-28 09:58:29 +0530

    Making counter's Component  independent of state so that it can be controlled by parent i.e counterComponent

--- counter-app/src/components/counterComponent.jsx
+++ counter-app/src/components/counterComponent.jsx
@@ -1,33 +1,28 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    value: this.props.counterProps.value,
-    key: this.props.counterProps.key,
-  };
   styles = {
     fontWeight: 20,
     fontSize: 20,
   };
-
+  counter = this.props.counterProps;
   render() {
     return (
       <React.Fragment>
         <span style={this.styles} className={this.getBadgeClasses()}>
-          {this.state.value}
+          {this.counter.value}
         </span>
-        {this.props.children}
         <button
           style={this.styles}
           className="btn btn-primary"
-          onClick={this.handleIncrement}
+          onClick={() => this.props.onIncrement(this.counter)}
         >
           Increment
         </button>
         <button
           style={this.styles}
           className="btn btn-danger m-2"
-          onClick={() => this.props.onDelete(this.state.key)}
+          onClick={() => this.props.onDelete(this.counter.key)}
         >
           Delete
         </button>
@@ -35,14 +30,9 @@ class Counter extends Component {
     );
   }
 
-  handleIncrement = () => {
-    console.log("called");
-    this.setState({ value: this.state.value + 1 });
-  };
-
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.value === 0 ? "primary" : "warning";
+    classes += this.counter.value === 0 ? "primary" : "warning";
     return classes;
   }
 }
--- counter-app/src/components/countersComponent.jsx
+++ counter-app/src/components/countersComponent.jsx
@@ -13,15 +13,35 @@ class Counters extends Component {
   render() {
     return (
       <React.Fragment>
+        <button className="btn btn-info m-3" onClick={this.handleReset}>
+          Reset
+        </button>
         {this.state.counters.map((counter) => (
           <div key={counter.key}>
-            <Counter counterProps={counter} onDelete={this.handleDelete} />
+            <Counter
+              counterProps={counter}
+              onDelete={this.handleDelete}
+              onIncrement={this.handleIncrement}
+            />
             <br />
           </div>
         ))}
       </React.Fragment>
     );
   }
+  handleIncrement = (counter) => {
+    console.log("handleIncrement called. with counter", counter);
+    const counters = [...this.state.counters];
+    const counterIndex = counters.indexOf(counter);
+    counters[counterIndex].value++;
+    this.setState({ counters: counters });
+  };
+  handleReset = () => {
+    const counters = [...this.state.counters];
+    counters.map((counter) => (counter.value = 0));
+    this.setState({ counters: counters });
+    console.log(counters);
+  };
   handleDelete = (key) => {
     const newCounters = this.state.counters.filter((counter) => {
       console.log(counter.key, key);
