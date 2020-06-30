6cd3cb6836e714f8a774e8a9ac038064edff3742
DurgeshKumar <durgeshkumar@Durgeshs-MacBook-Air.local>
2020-06-30 11:10:06 +0530

    basic routing complete

--- routing/src/component/aboutus.jsx
+++ routing/src/component/aboutus.jsx
@@ -18,40 +18,7 @@ class AboutUsComponent extends Component {
           <div class="col col-sm-12">
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
-              finibus consectetur lorem, a pellentesque lorem commodo id. Nullam
-              non lacinia ligula. Lorem ipsum dolor sit amet, consectetur
-              adipiscing elit. Mauris euismod justo vitae erat lobortis
-              fermentum. Etiam volutpat leo vel erat commodo, auctor varius nisi
-              rutrum. In non mollis erat, vel sagittis velit. Nunc porta
-              venenatis erat, quis fermentum eros tincidunt quis. Duis id
-              tincidunt enim. Sed eros velit, fermentum vel malesuada sit amet,
-              ultricies et metus. Nunc quis purus vitae arcu convallis interdum
-              a ut velit. Morbi suscipit viverra lectus, maximus efficitur
-              ligula faucibus nec. Suspendisse potenti. Lorem ipsum dolor sit
-              amet, consectetur adipiscing elit. Integer pharetra at enim
-              pretium aliquet. Nam ut nibh et nunc sodales volutpat et rutrum
-              nisl. Maecenas turpis dolor, blandit ac urna sed, euismod rhoncus
-              felis. Aliquam erat volutpat. Sed tempus luctus nibh, eu dictum
-              augue tristique sed. Class aptent taciti sociosqu ad litora
-              torquent per conubia nostra, per inceptos himenaeos. Integer ut
-              dolor in ligula blandit scelerisque ac id quam. Maecenas viverra
-              pretium erat. Aliquam sit amet sapien posuere, gravida erat sit
-              amet, ornare nunc. Maecenas sollicitudin dignissim volutpat.
-              Curabitur id volutpat mi. Praesent eleifend elit eu odio accumsan
-              mattis. Duis sit amet lectus quis magna porttitor venenatis.
-              Pellentesque ut fermentum nibh, vitae posuere leo. Sed sed nulla
-              tristique, porttitor enim a, tincidunt purus. In dolor risus,
-              ultricies ut pellentesque ullamcorper, commodo vulputate leo.
-              Integer ornare ornare tempus. Vestibulum ante ipsum primis in
-              faucibus orci luctus et ultrices posuere cubilia curae; Sed et
-              efficitur libero. Aenean eget pretium velit. Vestibulum mauris
-              consequat vitae enim. Quisque quam nisl, commodo dignissim neque
-              a, pharetra bibendum lectus. Sed ultrices ex eu massa blandit
-              accumsan. Donec vel magna velit. Morbi quis turpis augue. Fusce
-              consectetur justo vel nunc varius sodales. Cras eget scelerisque
-              lorem. In id nunc nunc. Aenean a ex tincidunt, ornare justo eget,
-              elementum tortor. Ut rhoncus vitae diam vitae faucibus. Nunc nec
-              quam eget dolor egestas luctus.
+              finibus consectetur lorem.
             </p>
           </div>
         </div>
--- routing/src/component/contactus.jsx
+++ routing/src/component/contactus.jsx
@@ -35,58 +35,7 @@ class ContactUsComponent extends Component {
               porttitor, nec molestie diam lacinia. Sed ut massa eget mi
               ultricies tempus. Duis rhoncus elit consectetur arcu malesuada, ut
               tristique orci mattis. Morbi consequat magna eget mauris porta,
-              nec tempor purus cursus. Mauris aliquet ante vel lorem vulputate,
-              id eleifend tellus tristique. Phasellus lobortis lectus eget erat
-              molestie iaculis. Pellentesque et diam scelerisque, commodo ante
-              eu, pharetra erat. Lorem ipsum dolor sit amet, consectetur
-              adipiscing elit. Cras finibus consectetur lorem, a pellentesque
-              lorem commodo id. Nullam non lacinia ligula. Lorem ipsum dolor sit
-              amet, consectetur adipiscing elit. Mauris euismod justo vitae erat
-              lobortis fermentum. Etiam volutpat leo vel erat commodo, auctor
-              varius nisi rutrum. In non mollis erat, vel sagittis velit. Nunc
-              porta venenatis erat, quis fermentum eros tincidunt quis. Duis id
-              tincidunt enim. Sed eros velit, fermentum vel malesuada sit amet,
-              ultricies et metus. Nunc quis purus vitae arcu convallis interdum
-              a ut velit. Morbi suscipit viverra lectus, maximus efficitur
-              ligula faucibus nec. Suspendisse potenti. Lorem ipsum dolor sit
-              amet, consectetur adipiscing elit. Integer pharetra at enim
-              pretium aliquet. Nam ut nibh et nunc sodales volutpat et rutrum
-              nisl. Maecenas turpis dolor, blandit ac urna sed, euismod rhoncus
-              felis. Aliquam erat volutpat. Sed tempus luctus nibh, eu dictum
-              augue tristique sed. Class aptent taciti sociosqu ad litora
-              torquent per conubia nostra, per inceptos himenaeos. Integer ut
-              dolor in ligula blandit scelerisque ac id quam. Maecenas viverra
-              pretium erat. Aliquam sit amet sapien posuere, gravida erat sit
-              amet, ornare nunc. Maecenas sollicitudin dignissim volutpat.
-              Curabitur id volutpat mi. Praesent eleifend elit eu odio accumsan
-              mattis. Duis sit amet lectus quis magna porttitor venenatis.
-              Pellentesque ut fermentum nibh, vitae posuere leo. Sed sed nulla
-              tristique, porttitor enim a, tincidunt purus. In dolor risus,
-              ultricies ut pellentesque ullamcorper, commodo vulputate leo.
-              Integer ornare ornare tempus. Vestibulum ante ipsum primis in
-              faucibus orci luctus et ultrices posuere cubilia curae; Sed et
-              efficitur libero. Aenean eget pretium velit. Vestibulum mauris
-              eros, rutrum ullamcorper suscipit vitae, sollicitudin sit amet
-              tortor. Pellentesque habitant morbi tristique senectus et netus et
-              malesuada fames ac turpis egestas. Aenean in pellentesque libero,
-              vel elementum lacus. Donec nec consectetur ligula. Donec ac
-              convallis dui. Fusce leo nisl, sodales a libero at, suscipit
-              iaculis quam. Proin condimentum orci eget varius vulputate.
-              Vivamus ultricies ipsum in pellentesque maximus. Duis est nibh,
-              efficitur et aliquet sed, vestibulum vitae ipsum. Pellentesque
-              pharetra, turpis a iaculis mattis, metus neque luctus sapien, eu
-              condimentum nibh magna in tellus. In in metus vehicula metus
-              viverra tempor. Donec eget mollis massa, in porta nisl.
-              Pellentesque quis ex condimentum, iaculis libero non, finibus
-              diam. Vestibulum ullamcorper mi nisi. Etiam sodales pretium eros,
-              nec varius ex. Maecenas quam felis, bibendum vel scelerisque id,
-              consequat vitae enim. Quisque quam nisl, commodo dignissim neque
-              a, pharetra bibendum lectus. Sed ultrices ex eu massa blandit
-              accumsan. Donec vel magna velit. Morbi quis turpis augue. Fusce
-              consectetur justo vel nunc varius sodales. Cras eget scelerisque
-              lorem. In id nunc nunc. Aenean a ex tincidunt, ornare justo eget,
-              elementum tortor. Ut rhoncus vitae diam vitae faucibus. Nunc nec
-              quam eget dolor egestas luctus.
+              nec tempor purus cursus.
             </p>
           </div>
         </div>
